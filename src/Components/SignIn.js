43ee291c535f904ab2142636386b3b80e533746c
Craig <craigclement@hotmail.com>
2022-05-18 17:48:38 +0100

    console.log token/user

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -13,8 +13,10 @@ function SignIn() {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
+        console.log(token)
         // The signed-in user info.
         const user = result.user;
+        console.log(user)
         // redux action? --> dispatch({ type: SET_USER, user });
       })
       .catch((error) => {
